0ac39059e446423ed84028d0ff1130f393b09dc9
khalid ali khan <khalidbinalikhan@gmail.com>
2024-03-02 16:41:17 +0500

    some changes in the model and html code

--- lazyshop/mainapp/models.py
+++ lazyshop/mainapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+STATE_CHOICES = (
+    ('Swat', 'Swat'),
+    ('Kalam', 'Kalam'),
+    ('Buneer', 'Buneer'),
+    ('Peshawar', 'Peshawar')
+)
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+
+    def __str__(self):
+        return self.name
+
